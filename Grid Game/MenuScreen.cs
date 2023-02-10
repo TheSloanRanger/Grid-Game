5f66a7a8ebead3092eaaa1e64cf31e3259de1673
Ben-Buchan <2453721@dundee.ac.uk>
2023-02-10 15:50:40 +0000

    Added a menu

---
+++ Grid Game/MenuScreen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grid_Game
+{
+    public partial class MenuScreen : Form
+    {
+        public MenuScreen()
+        {
+            InitializeComponent();
+            this.Text = "Connect 4";
+            this.BackColor = Color.LightGray;
+            this.Width = 900;
+            this.Height = 1000;
+
+        }
+
+        private void MenuScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            Form1 game = new Form1();
+            game.Show();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
