86bfeeb30957e19092d0a022bef2986c48cc2c04
naomisilver <2425281@dundee.ac.uk>
2023-02-10 08:03:08 +0000

    Added music + button to start and stop the music

--- Grid Game/Form1.cs
+++ Grid Game/Form1.cs
@@ -19,14 +19,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace Grid_Game
 {
     public partial class Form1 : Form
     {
         Button[,] btn = new Button[7, 6]; // array of buttons
-        Random r = new Random(); // new random reference (will potentially be used for "AI"
+        Button play = new Button();
+        Random r = new Random(); // new random reference (will potentially be used for "AI")
+
         int turn = 2; // global turn variable used to calculate which turn it is (can use this for win/lose checking, if turn == 44 then statemate)
+        int playPause = 1; // used to check if music is playing or not
 
         public Form1()
         {
@@ -34,10 +38,41 @@ namespace Grid_Game
             this.Text = "Connect 4";
             this.BackColor = Color.LightGray;
             this.Width = 900;
-            this.Height = 800;
-
+            this.Height = 1000;
+            play.SetBounds(700, 50, 100, 100);
+            play.BackColor = Color.ForestGreen;
+            play.Text = "Music!";
+            play.Font = new Font("Arial", 18, FontStyle.Bold);
+            play.Click += new EventHandler(play_Click);
+            Controls.Add(play);
             createGrid();
 
+        }
+
+        void play_Click(object sender, EventArgs e)
+        {
+            playMusic();
+        }
+
+        private void playMusic()
+        {
+            SoundPlayer music = new SoundPlayer("C:/Users/naomi/source/repos/Grid-Game/Grid Game/Tell-me-you-know-8Bit.wav");
+            // this file path will obviously throw and error any your system, just right click the .wav file within this folder and hit "copy as path", and replace the 
+            // backslashes with forward slashes. this was the only way I could figure out to get it to work, every other file path would just throw and error
+            if(playPause == 1)
+            {
+                music.Play();
+                play.Text = "Stop!";
+                play.BackColor = Color.OrangeRed;
+                playPause++;
+            }
+            else
+            {
+                music.Stop();
+                play.Text = "Play!";
+                play.BackColor = Color.ForestGreen;
+                playPause--;
+            }
         }
 
         void btnEvent_Click(object sender, MouseEventArgs e, int x, int y) // event handler function
@@ -89,7 +124,7 @@ namespace Grid_Game
                 for (int y = 0; y < 6; y++)
                 {
                     btn[x, y] = new Button(); 
-                    btn[x, y].SetBounds(100 + (100 * x), 75 + (100 * y), 100, 100); 
+                    btn[x, y].SetBounds(100 + (100 * x), 200 + (100 * y), 100, 100); 
                     btn[x, y].BackColor = Color.White;
                     int localX = x, localY = y; // provides current coordinates of the button pressed when it is clicked
                     btn[x, y].MouseDown += (s, ev) => btnEvent_Click(s, ev, localX, localY); // lambda expression to not need to specifiy data being passed
@@ -107,7 +142,7 @@ namespace Grid_Game
             }
             else
             {
-                return false; // returns false, due to turn not iterating is still the player's turn who attempted to misplace piece 
+                return false; // returns false, due to turn not iterating is still the player's turn who attempted the misplaced piece 
             }
         }
 
@@ -127,12 +162,12 @@ namespace Grid_Game
                 {
                     if (btn[x, y].BackColor == Color.Red && btn[x+1, y].BackColor == Color.Red && btn[x+2, y].BackColor == Color.Red && btn[x+3, y].BackColor == Color.Red)
                     {
-                        System.Diagnostics.Debug.WriteLine("Red Wins Horizontally");
+                        Console.WriteLine("Red Wins Horizontally");
                     }
 
                     else if (btn[x, y].BackColor == Color.Yellow && btn[x + 1, y].BackColor == Color.Yellow && btn[x + 2, y].BackColor == Color.Yellow && btn[x + 3, y].BackColor == Color.Yellow)
                     {
-                        System.Diagnostics.Debug.WriteLine("Yellow Wins Horizontally");
+                        Console.WriteLine("Yellow Wins Horizontally");
                     }
 
                  
@@ -146,12 +181,12 @@ namespace Grid_Game
                 {
                     if (btn[x, y].BackColor == Color.Yellow && btn[x, y + 1].BackColor == Color.Yellow && btn[x, y + 2].BackColor == Color.Yellow && btn[x, y + 3].BackColor == Color.Yellow)
                     {
-                        System.Diagnostics.Debug.WriteLine("Yellow Wins Vertically");
+                        Console.WriteLine("Yellow Wins Vertically");
                     }
 
                     else if (btn[x, y].BackColor == Color.Red && btn[x, y + 1].BackColor == Color.Red && btn[x, y + 2].BackColor == Color.Red && btn[x, y + 3].BackColor == Color.Red)
                     {
-                        System.Diagnostics.Debug.WriteLine("Red Wins Vertically");
+                        Console.WriteLine("Red Wins Vertically");
                     }
                 }
             }
@@ -164,12 +199,12 @@ namespace Grid_Game
                 {
                     if (btn[x, y].BackColor == Color.Red && btn[x-1, y+1].BackColor == Color.Red && btn[x-2, y+2].BackColor == Color.Red && btn[x-3, y+3].BackColor == Color.Red)
                     {
-                        System.Diagnostics.Debug.WriteLine("Red Wins Diagonally");
+                        Console.WriteLine("Red Wins Diagonally");
                     }
 
                     else if (btn[x, y].BackColor == Color.Yellow && btn[x - 1, y + 1].BackColor == Color.Yellow && btn[x - 2, y + 2].BackColor == Color.Yellow && btn[x - 3, y + 3].BackColor == Color.Yellow)
                     {
-                        System.Diagnostics.Debug.WriteLine("Yellow Wins Diagonally");
+                        Console.WriteLine("Yellow Wins Diagonally");
                     }
                 }
             }
@@ -182,12 +217,12 @@ namespace Grid_Game
                 {
                     if (btn[x, y].BackColor == Color.Red && btn[x+1, y-1].BackColor == Color.Red && btn[x + 2, y - 2].BackColor == Color.Red && btn[x + 3, y - 3].BackColor == Color.Red)
                     {
-                        System.Diagnostics.Debug.WriteLine("Red Wins Diagonally");
+                        Console.WriteLine("Red Wins Diagonally");
                     }
 
                     else if (btn[x, y].BackColor == Color.Yellow && btn[x+1, y-1].BackColor == Color.Yellow && btn[x + 2, y - 2].BackColor == Color.Yellow && btn[x + 3, y - 3].BackColor == Color.Yellow)
                     {
-                        System.Diagnostics.Debug.WriteLine("Yellow Wins Diagonally");
+                        Console.WriteLine("Yellow Wins Diagonally");
                     }
                 }
             }
